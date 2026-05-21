681b136325d5cc3ce7c793c6a32ba940022e6e77
torikachi <a3601000er@gmail.com>
2026-05-21 15:28:02 +0700

    Update

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("sidenav").style.bottom = "0";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("sidenav").style.bottom = "-100%";
+}
